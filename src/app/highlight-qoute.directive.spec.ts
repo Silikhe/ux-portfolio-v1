1992f23ec21386641b078aa6314f78955dc9132a
Silas Silikhe <65366942+Silikhe@users.noreply.github.com>
2023-02-18 12:06:26 +0300

    Delete src directory

--- src/app/highlight-qoute.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-import { HighlightQouteDirective } from './highlight-qoute.directive';
-
-describe('HighlightQouteDirective', () => {
-  it('should create an instance', () => {
-    const directive = Directive;
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-import { ɵBrowserPlatformLocation } from '@angular/platform-browser';
-
-export class Quote {
-  showInfo: boolean;
-  constructor(public id:number,public name:string,public title:string,public quote:string,public author:string, public datePosted: Date, public likes:number, public dislikes:number){
-      this.showInfo=false;
-    }
-}
